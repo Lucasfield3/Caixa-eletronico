c93e4001bec25860b4dd4522a4cb28dcc1c27af1
Lucasfield3 <lucasclic@yahho.com.br>
2020-10-27 14:12:20 -0300

    wraping

--- assets/js/saudacao.js
+++
@@ -1,8 +0,0 @@
-function saudacoes(){
-
-    const name = document.getElementById('name').value;
-    const lastName = document.getElementById('last-name').value;
-
-    alert(`${name} ${lastName} olá`);
-
-}
